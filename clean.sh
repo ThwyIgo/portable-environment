a60a144d6e03c3580ce633bd8e14af9d23120e7c
ThwyIgo <thiagopachecorocha@hotmail.com>
2022-09-20 18:14:17 -0300

    Generate .desktop files with icons; runNix function; better code organization; clean.sh

---
+++ clean.sh
@@ -0,0 +1,8 @@
+# Remove Nix
+chmod -R u+rw ~/.nix && rm -rf ~/.nix ~/.nix-profile ~/.nix-defexpr/ ~/.nix-channels ~/.local/bin/nix-user-chroot
+# Remove Fira Code
+rm ~/.local/share/fonts/ttf/FiraCode-* ~/.local/share/fonts/variable_ttf/FiraCode-* ~/.local/share/fonts/woff/FiraCode-* ~/.local/share/fonts/woff2/FiraCode-* ~/.local/share/fonts/{fira_code.css,README.txt,specimen.html}
+# Remove emacs
+rm -rf ~/.emacs.d/ ~/.local/share/applications/emacs-nix.*
+# Remove Icons
+rm -rf ~/.local/share/icons/
