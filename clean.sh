ca9a3e9d3ba4d1f38c345c553747a3baedcdb5ce
ThwyIgo <thiagopachecorocha@hotmail.com>
2022-11-30 09:35:03 -0300

    Fixed genDesktopFile and trash in installFonts

--- clean.sh
+++ clean.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/env bash
-
-# Warning: This script will remove your ~/.emacs.d/
-
-# Remove Nix
-chmod -R u+rw ~/.nix && rm -rf ~/.nix ~/.nix-profile ~/.nix-defexpr/ ~/.nix-channels ~/.local/bin/nix-user-chroot ~/.local/bin/runNix.sh
-# Remove Fira Code
-rm ~/.local/share/fonts/ttf/FiraCode-* ~/.local/share/fonts/variable_ttf/FiraCode-* ~/.local/share/fonts/woff/FiraCode-* ~/.local/share/fonts/woff2/FiraCode-* ~/.local/share/fonts/{fira_code.css,README.txt,specimen.html}
-# Remove emacs
-rm -rf ~/.emacs.d/ ~/.local/share/applications/emacs-nix.*
-# Remove clangd
-rm ~/.local/bin/clangd
-rm -rf ~/.local/lib/clang
-# Remove Icons
-rm -rf ~/.local/share/icons/
+#!/usr/bin/env sh
+chmod -R u+rw ~/.nix
+rm -rf /home/thiago/.nix /mnt/d/Arquivos/Docs/Universidade/portable-environment/array /home/thiago/.nix /home/thiago/.nix-profile /home/thiago/.nix-defexpr /home/thiago/.nix-channels /home/thiago/.local/bin/runNix.sh /home/thiago/.config/portable-env.sh /mnt/d/Arquivos/Docs/Universidade/portable-environment/Fira_Code.zip /home/thiago/.local/share/fonts/README.txt /home/thiago/.local/share/fonts/specimen.html /home/thiago/.local/share/fonts/fira_code.css /home/thiago/.local/share/fonts/ttf/FiraCode-Bold.ttf /home/thiago/.local/share/fonts/ttf/FiraCode-Light.ttf /home/thiago/.local/share/fonts/ttf/FiraCode-Medium.ttf /home/thiago/.local/share/fonts/ttf/FiraCode-Regular.ttf /home/thiago/.local/share/fonts/ttf/FiraCode-Retina.ttf /home/thiago/.local/share/fonts/ttf/FiraCode-SemiBold.ttf /home/thiago/.local/share/fonts/variable_ttf/FiraCode-VF.ttf /home/thiago/.local/share/fonts/woff/FiraCode-Bold.woff /home/thiago/.local/share/fonts/woff/FiraCode-Light.woff /home/thiago/.local/share/fonts/woff/FiraCode-Medium.woff /home/thiago/.local/share/fonts/woff/FiraCode-Regular.woff /home/thiago/.local/share/fonts/woff/FiraCode-SemiBold.woff /home/thiago/.local/share/fonts/woff/FiraCode-VF.woff /home/thiago/.local/share/fonts/woff2/FiraCode-Bold.woff2 /home/thiago/.local/share/fonts/woff2/FiraCode-Light.woff2 /home/thiago/.local/share/fonts/woff2/FiraCode-Medium.woff2 /home/thiago/.local/share/fonts/woff2/FiraCode-Regular.woff2 /home/thiago/.local/share/fonts/woff2/FiraCode-SemiBold.woff2 /home/thiago/.local/share/fonts/woff2/FiraCode-VF.woff2 /home/thiago/.local/LICENSE.TXT /home/thiago/.local/clangd.zip /home/thiago/.local/clangd_15.0.3/ /home/thiago/.local/bin/clangd /home/thiago/.local/share/applications/emacs-nix.desktop /home/thiago/.local/share/applications/emacs-nix.sh /home/thiago/.local/share/icons/emacs.svg /home/thiago/.emacs.d/ 
